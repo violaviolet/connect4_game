437ac87984bf9df6f7c6e23c47810af07ae5811f
Wiola <wioleta.swist@gmail.com>
2020-10-06 14:40:41 +0200

    users and board color modification were made

--- pygame_connect4/game_functions.py
+++ pygame_connect4/game_functions.py
@@ -20,16 +20,61 @@ def drop_disc_in_column(selected_column, board, user):
     return board
 
 
-def draw_user_move(board, screen):
+def draw_dropped_disc(board, screen):
     for row in range(0, 6):
         for column in range(0, 7):
             x = column * 50 + 25
             y = row * 50 + 25
 
             if board[row][column] == 1:
-                pygame.draw.circle(screen, (255, 0, 255), (x, y), 10)
+                pygame.draw.circle(screen, (255, 0, 0), (x, y), 10)
 
             elif board[row][column] == 2:
-                pygame.draw.circle(screen, (0, 10, 255), (x, y), 10)
+                pygame.draw.circle(screen, (0, 255, 0), (x, y), 10)
 
 
+def check_row_of_last_move(board, selected_column):
+    row_index = 0
+    for row in range(len(board)):
+        if board[row][selected_column] == 0:
+            row_index = row + 1
+    return row_index
+
+
+def is_vertical_four_in_line(board, selected_column, user):
+    win_counter = 0
+    for row in range(len(board)):
+        if board[row][selected_column] == user:
+            win_counter = win_counter + 1
+    if win_counter >= 4:
+        return True
+
+
+def is_horizontal_four_in_line(board, selected_column, user):
+    win_counter = 0
+    last_move_row_index = check_row_of_last_move(board, selected_column)
+    for column in range(len(board[last_move_row_index])):
+        if board[last_move_row_index][column] == user:
+            win_counter = win_counter + 1
+        else:
+            win_counter = 0
+    if win_counter >= 4:
+        return True
+
+
+def check_winner(board, selected_column, user):
+    if is_horizontal_four_in_line(board, selected_column, user) is True or is_vertical_four_in_line(board, selected_column, user) is True:
+        print("Congrats you won player:", user)
+    return True
+
+
+def draw_winner_info(user, screen):
+    font_object = pygame.font.SysFont("freesansbold", 26)
+    if user == 1:
+        information = "Winner is Player:"+ str(user)
+    elif user == 2:
+        information = "Winner is PLayer:" + str(user)
+    text_frame = font_object.render(information, True, (20, 255, 20))
+    text_rect = text_frame.get_rect()
+    text_rect_center = (75, 75)
+    screen. blit(text_frame, text_rect)
