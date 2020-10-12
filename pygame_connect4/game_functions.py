42d15dc7e52d1da7481a961ab2f784da94a1833d
Wiola <wioleta.swist@gmail.com>
2020-10-12 22:19:39 +0200

    corrections horizontal checking

--- pygame_connect4/game_functions.py
+++ pygame_connect4/game_functions.py
@@ -17,7 +17,7 @@ def drop_disc_in_column(selected_column, board, user):
         else:
             break
     board[empty_row][selected_column] = user
-    return board
+    return empty_row
 
 
 def draw_dropped_disc(board, screen):
@@ -33,33 +33,37 @@ def draw_dropped_disc(board, screen):
                 pygame.draw.circle(screen, (0, 255, 0), (x, y), 10)
 
 
-def check_row_of_last_move(board, selected_column):
-    row_index = 0
-    for row in range(len(board)):
-        if board[row][selected_column] == 0:
-            row_index = row + 1
-    return row_index
-
-
-def is_vertical_four_in_line(board, selected_column, user):
+def is_vertical_four_in_line(board, selected_row, selected_column, user):
     win_counter = 0
-    for row in range(len(board)):
+    for row in range(selected_row, len(board)):
         if board[row][selected_column] == user:
-            win_counter = win_counter + 1
-    if win_counter >= 4:
-        return True
-
-
-def is_horizontal_four_in_line(board, selected_column, user):
-    win_counter = 0
-    last_move_row_index = check_row_of_last_move(board, selected_column)
-    for column in range(len(board[last_move_row_index])):
-        if board[last_move_row_index][column] == user:
             win_counter = win_counter + 1
         else:
             win_counter = 0
     if win_counter >= 4:
         return True
+
+
+
+def is_horizontal_four_in_line(board, row, selected_column, user):
+    win_counter = 0
+    print(selected_column)
+    for column in range(selected_column-1, -1, -1):
+        print("LEFT", column)
+        if board[row][column] == user:
+            win_counter = win_counter + 1
+        else:
+            break
+    for column1 in range(selected_column, len(board[row])):
+        print("RIGTH", column1)
+        if board[row][column1] == user:
+            win_counter = win_counter + 1
+        else:
+            break
+    print(win_counter)
+    if win_counter >= 4:
+        return True
+
 
 
 def check_winner(board, selected_column, user):
@@ -69,12 +73,12 @@ def check_winner(board, selected_column, user):
 
 
 def draw_winner_info(user, screen):
-    font_object = pygame.font.SysFont("freesansbold", 26)
+    font_object = pygame.font.SysFont("freesansbold", 46)
     if user == 1:
         information = "Winner is Player:"+ str(user)
     elif user == 2:
         information = "Winner is PLayer:" + str(user)
     text_frame = font_object.render(information, True, (20, 255, 20))
     text_rect = text_frame.get_rect()
-    text_rect_center = (75, 75)
+    text_rect_center = (100, 100)
     screen. blit(text_frame, text_rect)
