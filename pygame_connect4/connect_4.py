0dfffe7043e304478a3b1bc97e2ac7435ffa93e9
Wiola <wioleta.swist@gmail.com>
2020-10-12 22:20:13 +0200

    cheking the winner and time for showing a winner added

--- pygame_connect4/connect_4.py
+++ pygame_connect4/connect_4.py
@@ -7,7 +7,7 @@ pygame.init()
 
 screen = pygame.display.set_mode((700, 600), 0, 32)
 pygame.display.set_caption("Connect 4")
-screen_color = (51, 51, 255)
+screen_color = (0, 10, 255)
 
 user = 1
 turn = 1
@@ -22,17 +22,23 @@ board = [[0, 0, 0, 0, 0, 0, 0],
 keys_numbers = {K_1: 0, K_2: 1, K_3: 2, K_4: 3, K_5: 4, K_6: 5, K_7: 6}
 screen.fill(screen_color)
 gf.draw_board(screen)
-while True:
+winner = 0
+while winner == 0:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         if event.type == KEYDOWN:
             selected_column = keys_numbers[event.key]
-            board = gf.drop_disc_in_column(selected_column, board, user)
-            gf.draw_user_move(board, screen)
+            row = gf.drop_disc_in_column(selected_column, board, user)
+            gf.draw_dropped_disc(board, screen)
 
-    pygame.display.update()
-    turn += 1
-    user = (user % 2) + 1
-    print(user)
+            if gf.is_horizontal_four_in_line(board, row, selected_column, user) or gf.is_vertical_four_in_line(board,row,selected_column, user):
+                gf.draw_winner_info(user, screen)
+                pygame.time.wait(400)
+                winner = user
+            turn += 1
+            user = (user % 2) + 1
+        pygame.display.update()
+
+
